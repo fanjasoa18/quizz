b91b58614867bdb586652c072fce0334a4ace61b
Fanjasoa Andriamanjaka <hei.fanjasoa@gmail.com>
2023-04-07 08:53:34 +0300

    feat: add data component

---
+++ src/components/QuizzData.js
@@ -0,0 +1,22 @@
+const questions = [
+    {
+        questionText: "Quels sont les trois pierres angulaire du react?",
+        answerOptions: [
+            { answerText: "components, state, virtual DOM", isCorrect: false },
+            { answerText: "components, state, props", isCorrect: true },
+            { answerText: "components, state, life cycle", isCorrect: false },
+            { answerText: "components, state, life cycle", isCorrect: false },
+        ],
+    },
+    {
+        questionText: "Quelle est l'approche utilisée par React pour la création d'interfaces utilisateur ?",
+        answerOptions: [ 
+            {answerText: "L'approche basée sur les modèles", "isCorrect": false} ,
+            {answerText: "L'approche basée sur les composants", "isCorrect": true },
+            {answerText: "L'approche basée sur les fonctions", "isCorrect": false}   
+        ]
+    }
+    
+];
+
+export default questions;
